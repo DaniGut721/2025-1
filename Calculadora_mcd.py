20ff26f449a0018d85960eeea47fa8b3f34eebf9
DaniGut721 <daniel.gutierrez-s@uniminuto.edu.co>
2025-02-19 15:12:57 -0500

    Update Calculadora_mcd.py
    listo

--- Calculadora_mcd.py
+++ Calculadora_mcd.py
@@ -7,4 +7,4 @@ num1 = int(input("Introduce el primer numero: "))
 num2 = int(input("Introduce el segundo numero: "))
 
 resultado = mcd(num1, num2)
-print(f"El MCD de {num1} y {num2} es {resultado}")
+print(f"El MCD de {num1} y {num2} es: {resultado}")
